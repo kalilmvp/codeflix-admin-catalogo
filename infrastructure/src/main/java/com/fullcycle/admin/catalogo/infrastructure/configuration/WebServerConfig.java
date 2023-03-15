46eb1dfcf08263d4eacc2d150a405ffb49f1a31b
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2023-03-14 21:52:12 -0300

    - Configured resources for testing and production

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,15 @@
+package com.fullcycle.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author kalil.peixoto
+ * @date 3/13/23 21:45
+ * @email kalilmvp@gmail.com
+ */
+@Configuration
+@ComponentScan("com.fullcycle.admin.catalogo")
+public class WebServerConfig {
+
+}
