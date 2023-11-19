9e580784fe388beec63e0271398881c84c27f69b
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2023-11-19 22:03:32 +0100

    - jackson unit testing ( updateca  )

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/models/UpdateCategoryRequestTest.java
@@ -0,0 +1,43 @@
+package com.fullcycle.admin.catalogo.infrastructure.models;
+
+import com.fullcycle.admin.catalogo.JacksonTest;
+import com.fullcycle.admin.catalogo.infrastructure.category.models.UpdateCategoryRequest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+/**
+ * @author kalil.peixoto
+ * @date 11/19/23 21:59
+ * @email kalilmvp@gmail.com
+ */
+@JacksonTest
+public class UpdateCategoryRequestTest {
+
+    @Autowired
+    private JacksonTester<UpdateCategoryRequest> json;
+
+    @Test
+    public void testUnmarshall() throws IOException {
+        final var expectedName = "Moviees";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var json = """
+                {
+                    "name": "%s",
+                    "description": "%s",
+                    "is_active": %s
+                }
+                """.formatted(expectedName, expectedDescription, expectedIsActive);
+        final var actualJson = this.json.parse(json);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("description", expectedDescription)
+                .hasFieldOrPropertyWithValue("active", expectedIsActive);
+    }
+}
