61a6c2d0b74aa96b080dc3e286a31ccaad87ea66
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2024-07-05 17:33:28 +0100

    feat: added Admin and Amqp Config to route to rabbitmq server

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/configuration/AmqpConfig.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/configuration/AmqpConfig.java
@@ -2,7 +2,9 @@ package com.fullcycle.admin.catalogo.infrastructure.configuration;
 
 import com.fullcycle.admin.catalogo.infrastructure.configuration.annotations.VideoCreatedQueue;
 import com.fullcycle.admin.catalogo.infrastructure.configuration.annotations.VideoEncodedQueue;
+import com.fullcycle.admin.catalogo.infrastructure.configuration.annotations.VideoEvents;
 import com.fullcycle.admin.catalogo.infrastructure.configuration.properties.amqp.QueueProperties;
+import org.springframework.amqp.core.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,16 +18,58 @@ import org.springframework.context.annotation.Configuration;
 public class AmqpConfig {
 
     @Bean
-    @ConfigurationProperties("amqp.queue.video-created")
+    @ConfigurationProperties("amqp.queues.video-created")
     @VideoCreatedQueue
     public QueueProperties videoCreatedQueueProperties() {
         return new QueueProperties();
     }
 
     @Bean
-    @ConfigurationProperties("amqp.queue.video-encoded")
+    @ConfigurationProperties("amqp.queues.video-encoded")
     @VideoEncodedQueue
     public QueueProperties videoEncodedQueueProperties() {
         return new QueueProperties();
+    }
+
+    @Configuration
+    static class Admin {
+
+        @Bean
+        @VideoEvents
+        Exchange videoEventsExchange(@VideoCreatedQueue QueueProperties props) {
+            return new DirectExchange(props.getExchange());
+        }
+
+        @Bean
+        @VideoCreatedQueue
+        Queue videoCreatedQueue(@VideoCreatedQueue QueueProperties properties) {
+            return new Queue(properties.getQueue());
+        }
+
+        @Bean
+        @VideoCreatedQueue
+        Binding videoCreatedQueueBinding(
+                @VideoEvents DirectExchange exchange,
+                @VideoCreatedQueue Queue queue,
+                @VideoCreatedQueue QueueProperties props
+        ) {
+            return BindingBuilder.bind(queue).to(exchange).with(props.getRoutingKey());
+        }
+
+        @Bean
+        @VideoEncodedQueue
+        Queue videoEncodedQueue(@VideoEncodedQueue QueueProperties properties) {
+            return new Queue(properties.getQueue());
+        }
+
+        @Bean
+        @VideoEncodedQueue
+        Binding videoEncodedQueueBinding(
+                @VideoEvents DirectExchange exchange,
+                @VideoEncodedQueue Queue queue,
+                @VideoEncodedQueue QueueProperties props
+        ) {
+            return BindingBuilder.bind(queue).to(exchange).with(props.getRoutingKey());
+        }
     }
 }
--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/configuration/annotations/VideoCreatedQueue.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/configuration/annotations/VideoCreatedQueue.java
@@ -2,9 +2,7 @@ package com.fullcycle.admin.catalogo.infrastructure.configuration.annotations;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.*;
