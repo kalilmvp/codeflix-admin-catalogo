291d841f4ad10063581ff04cc53ebcf9a863ac55
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2024-08-11 23:02:15 +0100

    feat: finished integrating with elk

--- sandbox/dispose.sh
+++ sandbox/dispose.sh
@@ -5,5 +5,7 @@ cd services
 docker-compose down
 cd ../app
 docker-compose down
+cd ../elk
+docker-compose down
 
 echo "Disposed containers...."
--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,14 +1,19 @@
 # create docker networks
 docker network create adm_videos_network
 docker network create adm_videos_services
+docker network create elastic
 
 # create folders and permissions
+sudo chown root app/filebeat/filebeat.docker.yml
 mkdir -m 777 .docker
 mkdir -m 777 .docker/keycloak
+mkdir -m 777 .docker/es01
+mkdir -m 777 .docker/filebeat
 
 docker-compose -f services/docker-compose.yml up -d
+docker-compose -f elk/docker-compose.yml up -d
 sleep 10
-docker-compose -f app/docker-compose.yml up -d
+#docker-compose -f app/docker-compose.yml up -d
 
 echo "Initializing containers...."
 
